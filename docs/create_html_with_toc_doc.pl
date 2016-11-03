d58fe004a8a1beef7cb598e82cd1d6856a15196a
tomog999 <tomog999@ubuntu200>
2016-11-03 11:03:52 +0100

    modifies "create_html_with_toc_doc.pl" so only one *.html file is saved, and it has TOC inserted

--- docs/create_html_with_toc_doc.pl
+++ docs/create_html_with_toc_doc.pl
@@ -8,22 +8,29 @@ use warnings;
 use HTML::Toc;
 use HTML::TocUpdator;
 
+use File::Copy qw/ move /;
 
 unless (scalar @ARGV == 1) {die qq|Error: at least one argument must be supplied to the script!\n|};
 
-my $input_file = shift @ARGV;
+my $input_txt_file = shift @ARGV;
 
-unless ( -e $input_file && -f _ )  {die qq|Error: input file does not exist, or is not a file!\n|};
-unless ( $input_file =~ /\.txt$/ ) {die qq|Error: input file must have a "*.txt" extension!\n|};
+unless ( -e $input_txt_file && -f _ )  {die qq|Error: input file does not exist, or is not a file!\n|};
+unless ( $input_txt_file =~ /\.txt$/ ) {die qq|Error: input file must have a "*.txt" extension!\n|};
 
-my $output_file;
-($output_file = $input_file) =~ s/txt$/html/; 
+my $generated_html_file;
+($generated_html_file = $input_txt_file) =~ s/txt$/html/; 
 
-my $txt2html_cmd = qq{txt2html -mosaic -infile $input_file -outfile $output_file};
+my $txt2html_cmd = qq{txt2html -mosaic -infile $input_txt_file -outfile $generated_html_file};
 system($txt2html_cmd);
 
 
 my $toc        = HTML::Toc->new();
 my $tocUpdator = HTML::TocUpdator->new();
 
-$tocUpdator->insertIntoFile($toc, $output_file, {outputFile => "test_file.html"});
+my $output_html_with_toc_file;
+($output_html_with_toc_file = $generated_html_file) =~ s/^doc/doc_toc/;
+$tocUpdator->insertIntoFile($toc, $generated_html_file, {outputFile => "$output_html_with_toc_file"});
+
+unlink $generated_html_file or warn "Could not unlink $generated_html_file $!";
+
+move($output_html_with_toc_file, $generated_html_file);
